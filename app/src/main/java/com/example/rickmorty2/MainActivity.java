d326cc446a4be753590a0dff5b78fec292dbbde6
Merve1212 <67706542+Merve1212@users.noreply.github.com>
2023-04-06 01:28:14 +0300

    Second Page Design

--- app/src/main/java/com/example/rickmorty2/MainActivity.java
+++ app/src/main/java/com/example/rickmorty2/MainActivity.java
@@ -1,28 +1,42 @@
 package com.example.rickmorty2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedEditor;
     Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        sharedEditor = sharedPreferences.edit();
+        TextView frstTime = findViewById(R.id.textView);
+        if (isItFirestTime()) {
+            frstTime.setText("Welcome!");
+        } else {
+            frstTime.setText("Hello!".toUpperCase());
+        }
+
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
+
 
         h.postDelayed(new Runnable() {
             @Override
@@ -33,5 +47,16 @@ public class MainActivity extends AppCompatActivity {
             }
         },10000);
 
+    }
+    //The Function to know app is stated first time or not?
+    public boolean isItFirestTime() {
+        if (sharedPreferences.getBoolean("firstTime", true)) {
+            sharedEditor.putBoolean("firstTime", false);
+            sharedEditor.commit();
+            sharedEditor.apply();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/rickmorty2/SecondAct.java
+++ app/src/main/java/com/example/rickmorty2/SecondAct.java
@@ -1,15 +1,21 @@
 package com.example.rickmorty2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Window;
 import android.os.Bundle;
+import android.os.Build;
+import android.view.WindowManager;
 
 public class SecondAct extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         getSupportActionBar().hide();
+        //It makes it full-screen page
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
